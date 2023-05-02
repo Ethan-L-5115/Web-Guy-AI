eba5c6879fdd49829f1d900490cc3b7aea3ea289
Razzino <1605355354157004@mil>
2023-05-02 15:45:33 -0600

    still in prog

--- python_scripts/cluster_to_file.py
+++ python_scripts/cluster_to_file.py
@@ -1,1 +1,10 @@
+import csv
 
+def write_clusters_to_file(cluster_list, file_path):
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['label', 'centroid', 'filepaths'])
+        for i, cluster in enumerate(cluster_list):
+            centroid = cluster[0]
+            filepaths = cluster[1:]
+            writer.writerow([i+1, centroid, *filepaths])
