8f392086c0fa52d96ae790d4c6242a40a443657c
Thomas Blalock <blalockthomasm@gmail.com>
2023-05-06 10:46:17 -0600

    fixed workflow. everything now works except write_cluster_to_file

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,20 +1,26 @@
 import os
 from feature_extraction import extract_features
-from crop_faces import crop_faces
+from crop_faces import get_faces
 from k_means_clustering import k_means_clustering
 
-# Path to folder with images
-folder_path = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/Desktop/test_me'
+# main.py will go through the entire pre-UI process of clustering and booting the
+# clusters and centroids to a file located in clustered_folder_path defined below
 
-# Extract features
-extract_features(folder_path)
+# Folder Paths
+raw_folder_path = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/Desktop/test_me/test_me_raw'
+face_folder_path = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/Desktop/test_me/face'
+features_folder_path = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/Desktop/test_me'
+clustered_folder_path = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/Desktop/test_me/clusters.csv'
 
-# Crop faces
-input_folder = folder_path
-output_folder = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/Desktop/test_me_cropped'
-crop_faces(input_folder, output_folder)
 
-# Perform k-means clustering
-features_file = 'features.csv'
+# Crop faces (Comment out when not in use)
+# get_faces(raw_folder_path, face_folder_path)
+
+
+# Extract features (Comment out when not in use)
+# extract_features(face_folder_path, features_folder_path)
+
+# Perform k-means clustering (Comment out when not in use)
 n_clusters = 3
-k_means_clustering(features_file, n_clusters)
+k_means_clustering(features_folder_path+'/features.csv',
+                   clustered_folder_path, n_clusters)
