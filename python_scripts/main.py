afa5319392995f07c430cf50df2abdf7742665f9
Thomas Blalock <blalockthomasm@gmail.com>
2023-05-06 15:57:31 -0600

    pulled it all together I think

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,7 +1,7 @@
 import os
 from feature_extraction import extract_features
 from crop_faces import get_faces
-from k_means_clustering import k_means_clustering
+from k_means_clustering import k_means_clustering, plot_dist_by_clust
 
 # main.py will go through the entire pre-UI process of clustering and booting the
 # clusters and centroids to a file located in clustered_folder_path defined below
@@ -14,11 +14,13 @@ clustered_folder_path = 'C:/Users/C25Thomas.Blalock/OneDrive - afacademy.af.edu/
 
 
 # Crop faces (Comment out when not in use)
-# get_faces(raw_folder_path, face_folder_path)
-
+get_faces(raw_folder_path, face_folder_path)
 
 # Extract features (Comment out when not in use)
-# extract_features(face_folder_path, features_folder_path)
+extract_features(face_folder_path, features_folder_path)
+
+# # Find optimal k
+# plot_dist_by_clust(features_folder_path+'/features.csv', 10, 2)
 
 # Perform k-means clustering (Comment out when not in use)
 n_clusters = 3
