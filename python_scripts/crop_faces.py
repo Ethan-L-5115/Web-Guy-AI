5c0792b0079349efa0219e9dc067f40b3e47a0fd
Thomas Blalock <blalockthomasm@gmail.com>
2023-05-07 14:48:27 -0600

    changed the threshold resolution

--- python_scripts/crop_faces.py
+++ python_scripts/crop_faces.py
@@ -53,7 +53,7 @@ def get_faces(input_folder, output_folder):
 
     # Call process_images to get the bounding box coordinates
     # process_images(raw images, model used, min resolution, min confidence)
-    image_boxes = process_images(input_folder, mtcnn, 150*150, 0.95)
+    image_boxes = process_images(input_folder, mtcnn, 9000, 0.90)
 
     # Crop the images using the returned bounding boxes
     # crop_faces(folder w/faces, output folder, bounding boxes list, additional margin as a ratio)
